8241d040341d8f8c86faa912ca0232d5ab8adb1f
hadi.acer <hudhorisan@gmailcom>
2023-04-02 07:18:02 +0800

    'first'

--- blast sql/BOA_V_BCC.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('1', '1', 'Reguler', '1', 'all@an.tv', '3100');
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('8', '4', 'TCI', '5', 'tci@an.tv', '3100');
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('6', '2', 'Bonus', '3', 'zulfikar@an.tv', '3100');
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('3', '1', 'Reguler', '3', 'zulfikar@an.tv', '3100');
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('4', '2', 'Bonus', '4', 'antv_bonus@an.tv', '3100');
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('9', '1', 'Reguler', '7', 'all_dbs@viacreativehub.com', '3113');
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('10', '1', 'Reguler', '8', 'all_caf@vivat.id', '3112');
-INSERT INTO "BOA_V_BCC" ("ID", "GROUP_ID", "GROUP_NAME", "BCC_ID", "MAIL_TO", "COMPANY_ID") VALUES ('11', '1', 'Reguler', '9', 'all_imc@imc.co.id', '3111');
