357d1b703b46b3ea794a679ebbeeb85fb5403882
hadi.acer <hudhorisan@gmailcom>
2023-04-02 22:50:00 +0800

    'first'

--- resources/views/page/UPDATEPROGRESSDEAL.blade.php
+++ resources/views/page/UPDATEPROGRESSDEAL.blade.php
@@ -69,7 +69,7 @@
 				            <?php	
 					           	if($activ['id_sponsor'] == $data->id) 
 					            {
-				            		echo $activ['sales_name'];
+				            		echo 'DEALS';
 				            	}
 				            ?>
 
