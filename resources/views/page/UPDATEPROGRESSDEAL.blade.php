464caad8f712f64819f523ce6d2417d164536883
hadi.acer <hudhorisan@gmailcom>
2023-04-02 21:05:09 +0800

    'first'

--- resources/views/page/UPDATEPROGRESSDEAL.blade.php
+++ resources/views/page/UPDATEPROGRESSDEAL.blade.php
@@ -52,7 +52,7 @@
 		            <td><center><label>{{$data->advertiser_product}}</label></center></td>
 		            <td>
 		            	@foreach($ts_sponsor_deals as $keyy => $activ)
-				                <?php echo $activ->id_sponsor; ?>
+				                <?php echo $activ->end_periode; ?>
 				        @endforeach
 		            </td>
 		        </tr>
